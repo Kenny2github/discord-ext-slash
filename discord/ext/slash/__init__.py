e3d37d8bf81bc08b01dd4e72cba220a4a6f3fe19
Kenny2github <kenny2minecraft@gmail.com>
2021-01-27 23:00:06 +0800

    Bump version

--- discord/ext/slash/__init__.py
+++ discord/ext/slash/__init__.py
@@ -62,7 +62,7 @@ import asyncio
 import discord
 from discord.ext import commands
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 class ApplicationCommandOptionType(IntEnum):
     """Possible option types. Default is ``STRING``."""
